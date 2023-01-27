c440cea3c1cf3b785d0781cdcc7bac007b9c815e
“LAMIASASAOUD” <sasaoud.lamia96@gmail.com>
2023-01-27 11:20:04 +0100

    ajout de plusieurs worker

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 docker network create dockerwork
 
 docker run --network=dockerwork -e PORT=8080 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker --name worker -d worker
+docker run --network=dockerwork -e PORT=8090 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker1 --name worker1 -d worker
+docker run --network=dockerwork -e PORT=8095 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker2 --name worker2 -d worker
+
 docker run --network=dockerwork -e PORT=3000 -e TASK=4  --name planner -d planner
 #docker run --name planner  -d planner
